e8f1add8fbe0725871a47af654c4d2a1179f2183
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2025-12-10 16:29:51 +0000

    support partial file completion

--- flags.help.sh
+++ flags.help.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 mlr help flags | grep '^-' | sed 's/ or -/|-/g' | sed -E 's/ +/\t/' | sed -E 's/ +/ /g' |
-    table_unjag.sh 1 $'\t' '|' | sed 's/\[/\\[/g' | sed 's/\]/\\]/g' |
+    ./table_unjag.sh 1 $'\t' '|' | sed 's/\[/\\[/g' | sed 's/\]/\\]/g' |
     sed -E 's/\t(.*)/[\1]/' | sed '/[.}]]$/!s/]$/…]/' > flags.help
 # add format conversion table
 mlr help flags | grep '^|' | grep -o -- '--[^ ]*' >> flags.help
 # add help shorthands
 mlr help topics | grep 'mlr -' | sed 's/ *mlr //' | sed 's/ = mlr /[/' | sed 's/$/]/' >> flags.help
-# make --from complete filenames
+# make --from complete filenames using state-based completion for proper substring matching
 # https://github.com/zsh-users/zsh-completions/blob/master/zsh-completions-howto.org
-sed -i '' '/^--from/s/$/:filename:_files/' flags.help
+sed -i '' '/^--from/s/$/: :->from_file/' flags.help
